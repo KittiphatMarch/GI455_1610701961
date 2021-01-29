bf10e3aa8fc3d13cdaadf405a98defa878afaf59
kittiphatmoed <kittiphat.moed@bumail.net>
2021-01-29 19:24:02 +0700

    Update Unity Project

---
+++ GI455_Project_/Assets/Scripts/Search_btn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Search_btn : MonoBehaviour
+{
+    public string searchbar;
+    public GameObject searchInput;
+    public GameObject result;
+
+    public void GetResult()
+    {
+        searchbar = searchInput.GetComponent<Text>().text;
+        string[] textlist = { "Hamburger", "Sandwish", "Noodle", "Steak", "Pizza" };
+
+        bool found = false;
+
+        for (int i = 0; i < textlist.Length; i++)
+        {
+            if (textlist[i] == searchbar)
+            {
+                found = true;
+                result.GetComponent<Text>().text = "[ " + searchbar + " ] is found";
+            }
+        }
+        if (!found)
+        {
+            result.GetComponent<Text>().text = "[ " + searchbar + " ] is not found";
+        }
+    }
+}
